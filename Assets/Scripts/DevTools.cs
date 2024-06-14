8f3e53d3ac543db76d9ca9f6a3fd683d2bfde4c8
Asteros Irons <Elijahairons@gmail.com>
2024-06-14 15:36:34 -0400

    SetSongNotes now plays the song, StopSettingNotes now stops the music, both leave a log note to notify user of their activation. Always place DevTools and Rhythm on Rhythm visualizer or they won't work right

--- Assets/Scripts/DevTools.cs
+++ Assets/Scripts/DevTools.cs
@@ -6,20 +6,51 @@ public class DevTools : MonoBehaviour
 {
     public Dictionary<string, Dictionary<float, char>> songbook;
     public Dictionary<float, char> activeSong;
+    public AudioSource audio;
+    public float timer;
 
     // Start is called before the first frame update
     void Start()
     {
+        audio = GetComponent<AudioSource>();
         songbook = new Dictionary<string, Dictionary<float, char>>();
+        timer = 0;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
+        //Make songs to be played
         if (Input.GetKey(KeyCode.LeftControl) && Input.GetKey(KeyCode.LeftAlt) && Input.GetKeyDown(KeyCode.Alpha3))
         {
             SetSongNotes("Song " + songbook.Count + 1);
         }
+
+        //Note 1
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            activeSong[timer + Time.deltaTime] = 'E';
+        }
+
+        //Note 2
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            activeSong[timer + Time.deltaTime] = 'A';
+        }
+
+        //Note 3
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            activeSong[timer + Time.deltaTime] = '_';
+        }
+
+        //Stop recording input
+        if (Input.GetKey(KeyCode.LeftControl) && Input.GetKey(KeyCode.LeftAlt) && Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            StopSettingNotes();
+        }
+
+        timer += Time.deltaTime;
     }
 
     /// <summary>
@@ -32,11 +63,17 @@ public class DevTools : MonoBehaviour
         Dictionary<float, char> notes = new Dictionary<float, char>();
 
         songbook[song] = notes;
+        activeSong = notes;
+        timer = 0;
+        audio.Play();
+        Debug.Log("Recording...");
     }
 
     void StopSettingNotes()
     {
         activeSong = null;
+        audio.Stop();
+        Debug.Log($"Stop setting notes: {activeSong}");
     }
 
     void PlaySong(string song)
