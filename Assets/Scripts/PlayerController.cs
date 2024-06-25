aafdf549ea5663ded5c9c0ccc08ba49b4241f0bb
Jaden Pfeilstifter <PfeilstifterJ@lakeland.edu>
2024-06-25 15:28:08 -0500

    Added a commented script for changing sprites while walking for when we get more walking sprites.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -19,6 +19,20 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         movementDirection = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        //if(Input.GetAxis("Horizontal") > Input.GetAxis("Vertical")
+        //{
+        // if(Input.GetAxis("Horizantal") > 0){
+        // *change sprite to right facing*
+        //}else{
+        // *change sprite to left facing*
+        // }
+        // }else{
+        //  if(Input.GetAxis("Vertical") > 0){  *I am not sure if 1 is up or -1 for vertical GetAxis*
+        //  *Change sprite to facing up*
+        // }else{
+        //  *Change sprite to down facing*
+        // }
+        //}
     }
 
     private void FixedUpdate()
