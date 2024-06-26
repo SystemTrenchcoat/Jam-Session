93c39bbb7128b398f1ad19c56f0c1b4237ad9f8f
Ayinde Abrams <xxxenouniverse@gmail.com>
2024-06-26 15:39:12 -0400

    Add files via upload
    WORK IN PROGRESS

---
+++ Assets/Scripts/BackgroundHandler.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BackgroundHandler
+{
+   //public static event Action OnExitGame; -> in Rhythm.cs
+
+   /*Or maybe make a list containing the backgrounds 
+     if more are gonna be added in the future*/ 
+   [SerializeField] private Sprite dayBackground;
+   [SerializeField] private Sprite nightBackground;
+   private bool isDay;
+   private SpriteRenderer spriteRenderer;
+
+   
+   private void Start()
+   {
+      Rhythm.OnExitGame += SetBackground();
+   }
+    private void SetBackground() 
+    {
+          if(isDay) 
+          {
+               spriteRenderer.Sprite = nightBacground;
+               isDay = !isDay;
+          }
+          else 
+          {
+               spriteRenderer.Sprite = dayBackground;
+               isDay = !isDay;
+          }
+    }
+}
